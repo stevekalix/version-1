780a18edbcec2b1c83d19f2eca53f56e997c6e18
stevekalix <msr962005@gmail.com>
2025-11-08 11:08:32 +0530

    update message veson-2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-//   base:process.env.VITE_BASE_URL||"version-1/"
-base:'./'
+  base:process.env.VITE_BASE_URL||"version-1/"
+// base:'./'
 })
